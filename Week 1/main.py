184af36fc4224ab26b5f1747fe9564190098aa27
Marcos <marcosmunozgonzalez95@gmail.com>
2023-03-08 21:14:41 +0100

    First neural network

--- Week 1/main.py
+++ Week 1/main.py
@@ -10,6 +10,7 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision.transforms import ToTensor
 import matplotlib.pyplot as plt
 import sys
+import torch.optim as optim
 
 print("Cuda is available:", torch.cuda.is_available())
 
@@ -24,7 +25,8 @@ test_data_dir= root_dir + '/test'
 img_width = 224
 img_height=224
 batch_size=4
-epochs = 1000
+# epochs = 2
+epochs = 50
 
 ### CREATE DATASET
 transformation = transform.Compose([
@@ -38,6 +40,9 @@ valid_dataset = torchvision.datasets.ImageFolder(root="MCV-M5-Team05/datasets/MI
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=True)
 
+
+print('Classes: ', train_dataset.classes)
+
 ## Show a sample of our training set
 train_features, train_labels = next(iter(train_loader))
 print(f"Feature batch shape: {train_features.size()}")
@@ -45,7 +50,72 @@ print(f"Labels batch shape: {train_labels.size()}")
 img = train_features[0].squeeze()
 img = torch.permute(img, (1,2,0))
 label = train_labels[0]
-plt.imshow(img, cmap="gray")
-plt.show()
-print(f"Label: {label}")
+# plt.imshow(img, cmap="gray")
+# plt.show()
+print(f"Label: {train_dataset.classes[label]}")
 
+### CREATE MODEL
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(59536, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 8)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+net = Net()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+### TRAIN MODEL
+for epoch in range(epochs):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(train_loader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 49 == 48:    # print every 49 mini-batches
+            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+            running_loss = 0.0
+
+print('Finished Training')
+
+### TEST MODEL
+correct = 0
+total = 0
+# since we're not training, we don't need to calculate the gradients for our outputs
+with torch.no_grad():
+    for data in valid_loader:
+        images, labels = data
+        # calculate outputs by running images through the network
+        outputs = net(images)
+        # the class with the highest energy is what we choose as prediction
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print(f'Accuracy of the network on the test images: {100 * correct // total} %')
